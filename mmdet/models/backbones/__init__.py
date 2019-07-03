1a55c340064a107d9d147682ac125e1b3ffb98d6
chy7074646 <2585278953@qq.com>
2019-07-03 03:28:50 +0000

    update with pva model which include pvaNet and pvaLite

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -2,5 +2,6 @@ from .resnet import ResNet, make_res_layer
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
 from .hrnet import HRNet
+from .pvanet import PVALiteFeat
 
-__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet', 'pvanet']
+__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet', 'PVALiteFeat']
--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
 
 ##########################test  VOCdata type################################
 #step 1. generate test_out.pkl
-#tools/dist_test.sh configs/faster_rcnn_r50_fpn_1x.py work_dirs/faster_rcnn_r50_fpn_1x/epoch_2.pth 4 --out work_dirs/faster_rcnn_r50_fpn_1x/test_out.pkl #--eval proposal proposal_fast bbox --show
+tools/dist_test.sh configs/faster_rcnn_r50_fpn_1x.py work_dirs/faster_rcnn_r50_fpn_1x/epoch_90.pth 4 --out work_dirs/faster_rcnn_r50_fpn_1x/test_out.pkl --eval bbox --show #--eval proposal proposal_fast bbox --show
 
 #step 2. get evaluate result according to the test_out.pkl
-python tools/voc_eval.py work_dirs/faster_rcnn_r50_fpn_1x/test_out.pkl configs/faster_rcnn_r50_fpn_1x.py
+#python tools/voc_eval.py work_dirs/faster_rcnn_r50_fpn_1x/test_out.pkl configs/faster_rcnn_r50_fpn_1x.py
